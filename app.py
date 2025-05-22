cc3a398f79d057f2cd1c52074777c1732b598402
Jack Noutch <jack.noutch@gmail.com>
2025-05-22 21:22:11 +0100

    Second view for officer search

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 # Third party imports
 import requests
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,13 +13,29 @@ load_dotenv()
 # Get the environment variable for the Companies House API key
 companies_house_api_key = os.getenv('COMPANIES_HOUSE_API_KEY')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    
-    company_number = os.getenv('COMPANY_ID')
-    api_url = f"https://api.company-information.service.gov.uk/company/{company_number}"
 
-    response = requests.get(api_url, auth=(companies_house_api_key, ''))
+    if request.method == 'POST':
+    
+        company_number = os.getenv('COMPANY_ID')
+        api_url = f"https://api.company-information.service.gov.uk/company/{company_number}/filing-history"
+
+        response = requests.get(api_url, auth=(companies_house_api_key, ''))
+        data = response.json()
+
+        return f"API response: {data}"
+
+    return "GET request received"
+
+@app.route('/officers/<string:query>', methods=['GET'])
+def officer(query):
+    api_url = f"https://api.company-information.service.gov.uk/search/officers"
+    response = requests.get(
+        api_url,
+        params={"q": query,},
+        auth=(companies_house_api_key, '')
+    )
     data = response.json()
 
     return f"API response: {data}"
