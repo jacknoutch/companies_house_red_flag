1ee1fc847c7ea40aeca5a5fc77c53df96009dcb6
Jack Noutch <jack.noutch@gmail.com>
2025-05-22 20:00:08 +0100

    API call success

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+# Standard library imports
+import os
+
+# Third party imports
+import requests
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
 
+load_dotenv()
+
+# Get the environment variable for the Companies House API key
+companies_house_api_key = os.getenv('COMPANIES_HOUSE_API_KEY')
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    
+    company_number = os.getenv('COMPANY_ID')
+    api_url = f"https://api.company-information.service.gov.uk/company/{company_number}"
+
+    response = requests.get(api_url, auth=(companies_house_api_key, ''))
+    data = response.json()
+
+    return f"API response: {data}"
